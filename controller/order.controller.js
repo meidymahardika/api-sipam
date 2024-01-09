def6950d41b7dc8ddcfb5848315a2ae1096cfbc8
meidymahardika <meidymahardika300501@gmail.com>
2024-01-09 11:09:23 +0700

    get queue

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -71,12 +71,9 @@ const orderController = {
     },
     getQueue: async (req, res) => {
         try {
-            const { page, perpage } = req.query
-            const pageNumber = (page-1)*perpage
             const [rows, fields] = await pool.query('SELECT MAX(queue) AS max_queue FROM tb_order;')
-            console.log(12, rows);
             const payload = { 
-                data: rows
+                data: rows[0]
             }
             response(200, payload, "SUCCESS", res)
         } catch (error) {
