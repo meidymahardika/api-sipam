636ee8648a29e1c40bc5751c745ce16665197709
meidymahardika <meidymahardika300501@gmail.com>
2024-01-09 08:40:31 +0700

    update order controller

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -5,15 +5,63 @@ const response = require('../utils/response')
 const orderController = {
     add: async (req, res) => {
         try {
-            const { idCategoryProduct, name, price, description, isActive } = req.body
+            const { orderNumber, queue, name, email, phone, paymentMethod, total } = req.body
 
             console.log(1, req.body);
 
-            // const sql = `INSERT INTO product(id_category_product, name, price, img, description, is_active, created_date, updated_date) VALUES ('${idCategoryProduct}', '${name}', ${price}, '${dateTime}_${downloadURL.split("&")[1]}', '${description}', ${isActive}, '${moment().format('YYYY-MM-DD')}', '${moment().format('YYYY-MM-DD')}')`;
-            // const [rows, fields] = await pool.query(sql, [idCategoryProduct, name, price, downloadURL, description, isActive])
-            // res.json({
-            //     data: rows
-            // })
+            const sql = `INSERT INTO tb_order(order_number, queue, name, email, phone, payment_method, total, status, created_at, updated_at) VALUES ('${orderNumber}', ${queue}, '${name}', '${email}', '${phone}', ${paymentMethod}, ${total}, 'WAITING', '${moment().format('YYYY-MM-DD')}', '${moment().format('YYYY-MM-DD')}')`;
+            await pool.query(sql, [orderNumber, queue, name, email, phone, paymentMethod, total], (err, result) => {
+                if (err) {
+                    console.error('Error inserting: ', err);
+                    return res.status(500).send('Error inserting data');
+                }
+            
+                console.log('Data inserted successfully');
+                res.json({
+                    data: result
+                })
+            })
+            const [id_order_rows, id_order_fields] = await pool.query(`SELECT id from tb_order ORDER BY id DESC LIMIT 1`)
+            const id_order = id_order_rows[0].id
+            req.body.detail.forEach((item,i) => {
+                const sql = `INSERT INTO order_detail(id_order, id_product, qty) VALUES (${id_order}, ${item.id}, ${item.qty})`;
+                pool.query(sql, [id_order, item.id, item.qty], (err, result) => {
+                    if (err) {
+                        console.error('Error inserting: ', err);
+                        return res.status(500).send('Error inserting data');
+                      }
+                
+                      console.log('Data inserted successfully');
+                      res.status(200).send('Data inserted successfully');
+                })
+            })
+            res.json({
+                data: 'success'
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                status: "error"
+            })
+        }
+    },
+    getList: async (req, res) => {
+        try {
+            const { page, perpage } = req.query
+            const pageNumber = (page-1)*perpage
+            const [rows, fields] = await pool.query(`SELECT product.*, order_detail.* FROM product JOIN order_detail ON product.id = order_detail.id_product LIMIT ${perpage} OFFSET ${pageNumber}`)
+            const [total_rows, total_fields] = await pool.query(`SELECT COUNT(*) AS total_rows FROM product`)
+            const total = total_rows[0].total_rows
+            const payload = { 
+                data: rows, 
+                meta: { 
+                    page: Number(page), 
+                    pages: Math.floor((total + (perpage - 1)) / perpage),
+                    perpage: Number(perpage), 
+                    total: total 
+                } 
+            }
+            response(200, payload, "SUCCESS", res)
         } catch (error) {
             console.log(error)
             res.json({
